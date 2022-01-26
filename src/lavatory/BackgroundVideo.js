a3391f16a791947031b89ff208e61e0d641543e7
rb-bbw <remo.bergamin@lernende.bbw.ch>
2022-01-26 18:02:58 +0100

    Add preview image to video

--- src/lavatory/BackgroundVideo.js
+++ src/lavatory/BackgroundVideo.js
@@ -1,6 +1,8 @@
+import preview from "./preview.png"
+
 export default function BackgroundVideo({src}) {
     return (
-        <video loop muted autoPlay>
+        <video loop muted autoPlay poster={preview}>
             <source src={src} type="video/mp4"/>
             Sorry, your browser doesn't support embedded videos.
         </video>
