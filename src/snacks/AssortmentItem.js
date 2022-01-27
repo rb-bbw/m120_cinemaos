e8f14be3dee5677888663c4dfabb638e40fad0d3
rb-bbw <remo.bergamin@lernende.bbw.ch>
2022-01-27 14:44:35 +0100

    Show placeholder while image is loading
    For some reason I can't put the function in helpers.js. Due to the
    deadline I have duplicated in every component that needs it (2).

--- src/snacks/AssortmentItem.js
+++ src/snacks/AssortmentItem.js
@@ -1,10 +1,25 @@
 import {Button, Card} from "react-bootstrap";
 import AddToCart from "./AddToCart";
+import placeholder from "../common/300x300.svg";
+import {useEffect, useState} from "react";
 
 export default function AssortmentItem({cart, setCart, item, categoryKey, itemKey}) {
+    let [img, setImg] = useState();
+
+    const fetchImage = async () => {
+        const res = await fetch(item.image);
+        const imageBlob = await res.blob();
+        const imageObjectURL = URL.createObjectURL(imageBlob);
+        setImg(imageObjectURL);
+    };
+
+    useEffect(() => {
+        fetchImage();
+    }, []);
+
     return (
         <Card>
-            <Card.Img src={item.image} alt={`Image of ${item.title}`}/>
+            <Card.Img src={typeof img === 'undefined' ? placeholder : img} alt={`Image of ${item.title}`}/>
             <Card.Body>
                 <Card.Title>{item.title}</Card.Title>
                 <p>{item.description}</p>
--- src/snacks/ItemDetail.js
+++ src/snacks/ItemDetail.js
@@ -1,14 +1,27 @@
 import {useParams} from "react-router-dom";
 import {assortment} from "./AssortmentService";
 import {Col, Image, Row} from "react-bootstrap";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import AddToCart from "./AddToCart";
+import placeholder from "../common/300x300.svg";
 
 export default function ItemDetail({cart, setCart}) {
+    let [img, setImg] = useState();
     let params = useParams();
     let item = assortment[params.category][params.item];
 
     useEffect(() => { document.title = item.title; })
+
+    const fetchImage = async () => {
+        const res = await fetch(item.image);
+        const imageBlob = await res.blob();
+        const imageObjectURL = URL.createObjectURL(imageBlob);
+        setImg(imageObjectURL);
+    };
+
+    useEffect(() => {
+        fetchImage();
+    }, []);
 
     return (
         <Row>
@@ -18,7 +31,7 @@ export default function ItemDetail({cart, setCart}) {
                 <AddToCart cart={cart} setCart={setCart} item={item} />
             </Col>
             <Col key="image" lg="auto">
-                <Image src={item.image} fluid/>
+                <Image src={typeof img === 'undefined' ? placeholder : img} fluid/>
             </Col>
         </Row>
     );
